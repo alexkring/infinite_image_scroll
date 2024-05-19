9d1dcd068ac23b4834e3d9e8ab910b613b75ec1e
Alexander w Kring <alexkring@Alexanders-MacBook-Pro.local>
2024-05-18 19:51:56 -0700

    Invoke imageapi from application

--- InfiniteImageScroll/Assets/ImageAPI.cs
+++ InfiniteImageScroll/Assets/ImageAPI.cs
@@ -3,26 +3,25 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 
-// Loads a mock set of images from a json file, for testing the image api.
+// Loads a mock set of images from https://hyper-image-test.netlify.app/.netlify/functions/images
 public class ImageAPI : IImageAPI
 {
     private List<ImageModel> _imageModelList;
 
-    public ImageAPI(string jsonFilepath) {
+    private const string kBaseUrl = "https://hyper-image-test.netlify.app/.netlify/functions/images";
+
+    public ImageAPI() {
         _imageModelList = new List<ImageModel>();
     }
     
     // Fetch limit# of image models 
     public async Task<List<IImageModel>> FetchImages(int offset, int limit) {
-        if (offset + limit > _imageModelList.Count) {
-            Debug.LogError($"could not fetch images because you are trying to fetch beyond the bounds of the images in the mock repo");
-            return null;
-        }
         List<IImageModel> imageModelsFound = new List<IImageModel>(limit);
-        for (int i = offset; i < limit; i++) {
-            ImageModel model = _imageModelList[i];
-            imageModelsFound.Add(model);
-        }
+        ImageModel model = new ImageModel();
+        model.Name = "";
+        model.Id = "";
+        model.Url = "";
+        imageModelsFound.Add(model);
         return imageModelsFound;
     }
 }
--- InfiniteImageScroll/Assets/ImageScrollApplication.cs
+++ InfiniteImageScroll/Assets/ImageScrollApplication.cs
@@ -1,28 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 // Used to initialize the application with the correct state, and to handle any unity event lifecycle management to communicate with other code components.
 public class ImageScrollApplication : MonoBehaviour
 {
     private ImageLoader _imageLoader;
-    private MockImageAPI _imageApi;
+    private ImageAPI _imageApi;
 
     void Awake() {
-        const string ImageApiFilePath = "file.json";
         _imageLoader = new ImageLoader();
-        _imageApi = new MockImageAPI(ImageApiFilePath);
+        _imageApi = new ImageAPI();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        StartCoroutine(FetchImages());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator FetchImages() {
+        Task<List<IImageModel>> task = _imageApi.FetchImages(0, 5);
+        yield return new WaitUntil(() => task.IsCompleted);
+        List<IImageModel> imageModels = task.Result;
     }
 }
